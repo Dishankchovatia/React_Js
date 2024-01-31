ae8d0d3690724f88ae4294279724c3c77b84a071
Dishankchovatia <dishankpatel03@gmail.com>
2024-01-31 16:23:55 +0530

    useCounter - custom counter hook

--- custom-hook-1/src/hooks/use-counter.js
+++ custom-hook-1/src/hooks/use-counter.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import {  useState, useEffect  } from 'react';
 
 const useCounter = (forwards = true) => {
   const [counter, setCounter] = useState(0);
@@ -18,4 +18,5 @@ const useCounter = (forwards = true) => {
   return counter;
 };
 
+// usecounter hook
 export default useCounter;
